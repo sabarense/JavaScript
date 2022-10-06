930fa5835893faaa256efdb95d7b7bc1c6ab3d3e
Yan Sabarense <sabarensey@gmail.com>
2022-10-06 15:58:51 -0300

    operadores aritmeticos

---
+++ arithmeticOperators/concatenate.js
@@ -0,0 +1,5 @@
+let nome = "Yan";
+let sobrenome = "Sabarense";
+let nomeCompleto = nome + " " + sobrenome;
+
+console.log(nomeCompleto);
---
+++ arithmeticOperators/typeCoercion.js
@@ -0,0 +1,16 @@
+let a = "5" + 2;
+console.log(`O valor de a é ${a} e o seu tipo é ${typeof a}`);
+
+let b = "5" - 2;
+console.log(`O valor de b é ${b} e o seu tipo é ${typeof b}`);
+
+let c = +"5" + 2;
+console.log(`O valor de a é ${c} e o seu tipo é ${typeof c}`);
+
+
+/*
+resultado:
+O valor de a é 52 e o seu tipo é string
+O valor de b é 3 e o seu tipo é number
+O valor de b é 7 e o seu tipo é number
+*/
