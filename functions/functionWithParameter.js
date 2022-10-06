786381bc74bd7ec5ccf4f86e77d4c01f874810ce
Yan Sabarense <sabarensey@gmail.com>
2022-10-06 18:35:07 -0300

    funções

---
+++ functions/functionWithParameter.js
@@ -0,0 +1,23 @@
+function mensagem(nome){
+    console.log("Olá, " + nome + "!");
+}
+mensagem("Yan");
+
+function soma(a,b){
+    console.log(a+b);
+}
+soma(3,4);
+
+/*
+function nome() {
+  instruções
+}
+*/
+
+/*
+function nome(parâmetros) {
+    instruções
+  }
+  
+  nome(parâmetros);
+*/
