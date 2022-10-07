2a865ec6328e99634964d98acfac277b52a3f351
Yan Sabarense <sabarensey@gmail.com>
2022-10-07 16:59:42 -0300

    vetores

---
+++ arrays/vectorInsertionAndRemoval.js
@@ -0,0 +1,4 @@
+let ultimo = vetor.pop();     // remove e retornar o último elemento do vetor
+vetor.push(ultimo);           // insere um elemento no fim do vetor
+let primeiro = vetor.shift(); // remove e retornar o primeiro elemento do vetor
+vetor.unshift(primeiro);      // insere um elemento na primeira posição do vetor
